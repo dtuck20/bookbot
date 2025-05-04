9455a37a2c289962f33dcacb15bb940a169dc894
dtuck20 <dtuck93@gmail.com>
2025-05-03 23:02:43 -0400

    add sys input variable to main.py. add comments

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stats import count_words
 from stats import count_characters
 from stats import sort_character_count
+import sys
 
+#gets the text of a txt file as a string
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
@@ -9,21 +11,30 @@ def get_book_text(path):
     return file_contents
 
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt")) 
-    character_count = count_characters(get_book_text("books/frankenstein.txt"))
+    if len(sys.argv) != 2:
+        #usage instructions if not expected input
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    #get book path from input variable
+    book_path = sys.argv[1]
 
-    #print(character_count)
+    #get the number of words in the book specified
+    num_words = count_words(get_book_text(book_path)) 
 
+    #get a dictionary consisting of each character used and their count of use in the book specified
+    character_count = count_characters(get_book_text(book_path))
+
+    #get a sorted list of dictionaries for each character in the book specified in descending order
     character_report = sort_character_count(character_count)
 
+    #create output report
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    #print(character_report)
+    #only print if the character is alphabetic
     for char in character_report:
-        #print(char)
         if char["char"].isalpha():
             letter = char["char"]
             count = char["num"]
--- stats.py
+++ stats.py
@@ -1,3 +1,4 @@
+#function that counts the number of words in the input string
 def count_words(text):
     words = text.split()
     count = 0
@@ -5,11 +6,16 @@ def count_words(text):
         count += 1
     return count
 
+#function that returns a dictionary containing each character used in the input string and how many times it is used
 def count_characters(text):
+    #set all lowercase in order to get one result per letter
     text = text.lower()
     words = text.split()
 
     count = {}
+    #for each word, check if each letter is already in the count dictionary. 
+    #if existing in dictionary, add to the count
+    #if not in dictionary, add to the dictionary with the count of 1
     for word in words:
         for letter in word:
             if letter in count:
@@ -18,19 +24,21 @@ def count_characters(text):
                 count[letter] = 1
     return count
 
+#sorting key for the list of dictionaries
 def sort_on(dict):
     return dict["num"]
 
-
+#returns a sorted list of dictionaries for each character used in the input string. Sorted in descending order by the number of times the character is used
 def sort_character_count(character_counts):
     character_dictionaries = []
 
-
+    #for each character in the input dictionary, create it's own dictionary containing the character and the count of use
     for character in character_counts:
         char_count = character_counts[character]
-        #print(character)
         character_dictionary = {"char": character,
                                 "num": char_count}
+        #add each new dictionary to the list of dictionaries called character_dictionaries
         character_dictionaries.append(character_dictionary)
+        #sort the list of dictionaries in descending order
         character_dictionaries.sort(reverse=True, key=sort_on)
     return character_dictionaries
